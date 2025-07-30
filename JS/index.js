681f0694368524799269a8c5458f5375f5610077
Geneen <geneen850@gmail.com>
2025-07-30 17:47:53 +0300

    add APOD facts fetch handler to /random route

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.body.prepend(renderNavbar());
   renderHome();
-  fetchFactsForSameDay();
 });
 
 const NASA_SEARCH_API = "https://images-api.nasa.gov/search";
@@ -905,6 +904,10 @@ const renderRoute = () => {
     case "/albums":
       renderAlbums();
       break;
+    case "/random":
+      fetchFactsForSameDay();
+      break;
+      
     default:
       renderHome();
       break;
